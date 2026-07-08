020726a87dc452d8a77502142042387f946a2b16
naveenraj0320-tech <naveenraj0320@gmail.com>
2026-07-08 19:50:09 +0530

    Today's update

---
+++ q2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class q2 {
+    public static void main(String agrs[]) {
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        String address = scan.nextLine();
+        int age = scan.nextInt();
+        System.out.println("my name is " + name);
+        System.out.println("my address is " + address);
+        System.out.println("my age is " + age);
+        
+    }
+}
